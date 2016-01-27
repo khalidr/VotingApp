cbb815adfc0876ee1b40dc615e7231c447a2d3d5
khalidr <k.o.reid@gmail.com>
2016-01-27 13:32:08 -0500

    cleanup

--- src/main/scala/com/kor/Main.scala
+++ src/main/scala/com/kor/Main.scala
@@ -1,6 +1,7 @@
 package com.kor
 
 import java.net.URL
+import java.util.concurrent.TimeUnit
 
 import akka.actor.{ActorSystem, Props}
 import com.kor.actors.WebserviceActor
@@ -10,11 +11,12 @@ import com.typesafe.scalalogging.StrictLogging
 import reactivemongo.api.MongoDriver
 
 import scala.concurrent.Await
-import scala.concurrent.duration.Duration
+import scala.concurrent.duration.{Duration, FiniteDuration}
 
 object Main extends App with StrictLogging{
 
-  implicit val actorSystem = ActorSystem()  //using default pool is not always the best solution
+  implicit val actorSystem = ActorSystem()
+  import actorSystem.dispatcher  //using default pool is not always the best solution
 
   val fileShareService = new DropBoxService(new URL(Settings.dropBoxUploadUrl), new URL(Settings.dropBoxMetaDataUrl), Settings.dropBoxToken)
   val mongoDriver = new MongoDriver()
@@ -23,12 +25,17 @@ object Main extends App with StrictLogging{
 
   actorSystem.actorOf(Props(new WebserviceActor(Settings.host, Settings.port, fileShareService, repo)))
 
-  logger.info(s"Listening on ${Settings.port}")
+  logger.info(s"Listening on port ${Settings.port}")
 
   sys.addShutdownHook {
+    implicit val timeout = FiniteDuration(2, TimeUnit.SECONDS)
     actorSystem.log.info("Shutting down")
+    Await.result(mongoConnection.askClose(), Duration.Inf)
+    logger.info("Mongo connection shutdown.")
+
     actorSystem.terminate()
     Await.result(actorSystem.whenTerminated, Duration.Inf)
     logger.info(s"Actor system '${actorSystem.name}' successfully shut down")
+
   }
 }
